f68d878f9358b7e2f13e0f1fcf0c60e90cfd3c24
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-02-23 22:59:03 -0300

    se crea nuevas vistas dashboard y usefavorite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import {BrowserRouter,Route,Routes} from "react-router-dom"
+import {BrowserRouter,Navigate,Route,Routes} from "react-router-dom"
 import React from 'react'
 import Home from "./views/Home"
 import AllData from "./views/AllData"
 import { Navbar } from "./components/Navbar";
 import { User } from "./views/User";
 import NotFound from "./views/NotFound";
+import UserFavorite from "./views/UserFavorite";
+import Dashboard from "./views/Dashboard";
 
 export default function App (){
   return (
@@ -14,6 +16,9 @@ export default function App (){
         <Route path="/" element={<Home/>} />
         <Route path="/all_data" element={<AllData/>} />
         <Route path="/user" element={<User/>} />
+        <Route path="/usuarios" element={<Navigate to="/user"/>} />
+        <Route path="/user/:id" element={<UserFavorite/>} />
+        <Route path="/dashboard/*" element={<Dashboard/>} />
         <Route path='*' element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
