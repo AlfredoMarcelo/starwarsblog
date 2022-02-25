c515c209c3414f0b79d059a50f61ba8a3c457c2e
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-02-24 23:45:08 -0300

    se crear componentes CardCharacter, CardVehicles, CardsPlanets

---
+++ src/components/CardCharacters.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardCharacters = () => {
+  return (
+    <div>CardCharacter</div>
+  )
+}
+
+export default CardCharacters
---
+++ src/components/CardVehicles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardVehicles = () => {
+  return (
+    <div>CardVehicles</div>
+  )
+}
+
+export default CardVehicles
